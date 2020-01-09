bdb67160a8e2f98edbda87f3943d6418dca15d4e
Nicole Kauer <nmekauer@gmail.com>
2020-01-09 12:05:57 -0800

    Change to Sys.sleep and move down to after first snapshot

--- tests/testthat/apps/shinytestexample/tests/mytest.R
+++ tests/testthat/apps/shinytestexample/tests/mytest.R
@@ -1,8 +1,8 @@
 app <- ShinyDriver$new("../")
 app$snapshotInit("mytest")
 
-app$waitFor("false", timeout = 2000)
 app$snapshot()
+Sys.sleep(5)
 app$setInputs(hi = "click")
 app$snapshot()
 app$setInputs(hi = "click")
