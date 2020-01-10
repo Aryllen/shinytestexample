f708f76b1fb700df1dfba68c45dac5d4767088ed
Nicole Kauer <nmekauer@gmail.com>
2020-01-09 16:31:09 -0800

    Attempt "easy" login

--- tests/testthat/test-shiny-app.R
+++ tests/testthat/test-shiny-app.R
@@ -5,5 +5,8 @@ library(shinytest)
 test_that("Shiny app works", {
   skip_on_cran()
 
+  syn <- attempt_instantiate()
+  attempt_login(syn = syn)
+
   expect_pass(testApp("apps/shinytestexample/", compareImages = FALSE))
 })
